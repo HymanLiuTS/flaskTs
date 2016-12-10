78a2ca2ac25dd20e93228dfccceca6d21a70563e
HymanLiu1987 <879651072@qq.com>
2016-12-10 08:51:03 +0800

    用flask开发个人博客（7）—— flask中设置和获取cookie

--- test.py
+++ test.py
@@ -1,30 +1,38 @@
 #coding:utf-8
-from flask import Flask,session,make_response,redirect,abort
+from flask import Flask,session,make_response,redirect,abort,request,render_template
 from flask_script import Manager
+import datetime
 app=Flask(__name__)
 manager=Manager(app)
 
-@app.route('/')
-def index():
-    return 'bad request',400
-
-@app.route('/response1')
-def response1():
-    response=make_response('set cookies')
-    response.set_cookie('hyman','123')
+@app.route('/set_cookie')
+def set_cookie():
+    response=make_response('Hello World');
+    outdate=datetime.datetime.today() + datetime.timedelta(days=30)
+    response.set_cookie('Name','Hyman',expires=outdate)
     return response
 
-@app.route('/response2')
-def response2():
-    return redirect('http://www.baidu.com')
+@app.route('/get_cookie')
+def get_cookie():
+    name=request.cookies.get('Name')
+    return name
 
-@app.route('/error')
-def error():
-    abort(404)
+@app.route('/get_template')
+def get_template():
+    return render_template('test.html')
 
-@manager.command
-def print_str():
-    print 'hello world'
+
+@app.route('/del_cookie')
+def del_cookie():
+    response=make_response('delete cookie')
+    response.set_cookie('Name','',expires=0)
+    return response
+
+@app.route('/del_cookie2')
+def del_cookie2():
+    response=make_response('delete cookie2')
+    response.delete_cookie('Name')
+    return response
 
 if __name__=='__main__':
     manager.run()
