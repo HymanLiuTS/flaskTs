53fbee80a116506c84e3b6968d56b2b411c72cdf
HymanLiu1987 <879651072@qq.com>
2016-12-10 14:01:25 +0800

    用flask开发个人博客（13）—— 链接静态资源

--- test.py
+++ test.py
@@ -71,7 +71,7 @@ def forfunc():
 def page_not_found(e):
     return render_template('404.html'),404
 
-@app.errorhandler(500):
+@app.errorhandler(500)
 def internal_server_error(e):
     return render_template('500.html'),500
 
