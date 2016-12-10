e28ac9e431261cc467b744bec5578540b6da69f8
HymanLiu1987 <879651072@qq.com>
2016-12-10 14:15:46 +0800

    用flask开发个人博客（14）—— flask中本地化时间的引用

--- test.py
+++ test.py
@@ -1,36 +1,15 @@
 #coding:utf-8
 from flask import Flask,session,make_response,redirect,abort,request,render_template
 from flask_script import Manager
+from flask_moment import Moment
 import datetime
 app=Flask(__name__)
 manager=Manager(app)
-
-
-class Myobj(object):
-    def __init__(self,name):
-        self.name=name
-
-    def getname(self):
-        return self.name
-
-
-
-@app.route('/')
-def index():
-    mydict={'key1':'123','key':'hello'}
-    mylist=(123,234,345,789)
-    myintvar=0
-    myobj=Myobj('Hyman')
-    return render_template('param.html',mydict=mydict,mylist=mylist,myintvar=0,myobj=myobj)
-
-@app.route('/user/<name>')
-def user(name):
-    return render_template('user.html',name=name)
-
+moment=Moment(app)
 
 @app.route('/set_cookie')
 def set_cookie():
-    response=make_response('set_cookie');
+    response=make_response('Hello World');
     outdate=datetime.datetime.today() + datetime.timedelta(days=30)
     response.set_cookie('Name','Hyman',expires=outdate)
     return response
@@ -44,36 +23,10 @@ def get_cookie():
 def get_template():
     return render_template('test.html')
 
+@app.route('/moment')
+def get_moment():
+    return render_template('moment.html')
 
-@app.route('/del_cookie')
-def del_cookie():
-    response=make_response('delete cookie')
-    response.set_cookie('Name','',expires=0)
-    return response
-
-@app.route('/del_cookie2')
-def del_cookie2():
-    response=make_response('delete cookie2')
-    response.delete_cookie('Name')
-    return response
-
-
-@app.route('/if')
-def iffunc():
-    return render_template('if.html')
-
-@app.route('/for')
-def forfunc():
-    comments=['nihao','123','asd','zxc']
-    return render_template('for.html',comments=comments)
-
-@app.errorhandler(404)
-def page_not_found(e):
-    return render_template('404.html'),404
-
-@app.errorhandler(500)
-def internal_server_error(e):
-    return render_template('500.html'),500
 
 if __name__=='__main__':
     manager.run()
