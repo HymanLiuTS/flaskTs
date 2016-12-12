fa3a927fb9639c68cf46de0a800a3436a4a3a9e8
HymanLiu1987 <879651072@qq.com>
2016-12-12 23:26:20 +0800

    用flask开发个人博客（18）—— 数据库管理之模型的创建

--- test.py
+++ test.py
@@ -5,6 +5,9 @@ from flask_moment import Moment
 from flask_wtf import FlaskForm
 from wtforms import StringField,SubmitField
 from wtforms.validators import Required
+from flask_sqlalchemy import SQLAlchemy
+import os
+basedir=os.path.abspath(os.path.dirname(__file__))
 
 
 import datetime
@@ -12,6 +15,26 @@ app=Flask(__name__)
 app.config['SECRET_KEY']='secret_key'
 manager=Manager(app)
 moment=Moment(app)
+app.config['SQLALCHEMY_DATABASE_URI']='sqlite:///'+os.path.join(basedir+'data.sqlite')
+app.config['SQLALCHEMY_COMMIT_ON_TEARDOWN']=True
+db=SQLAlchemy(app)
+
+class Role(db.Model):
+    __tablename__='roles'
+    id=db.Column(db.Integer,primary_key=True)
+    name=db.Column(db.String(64),unique=True)
+
+    def __repr__(self):
+        return '<Role %s>'%self.name
+
+class User(db.Model):
+    __tablename__='users'
+    id=db.Column(db.Integer,primary_key=True)
+    name=db.Column(db.String(64),unique=True)
+
+    def __repr__(self):
+        return '<User %s>'%self.name
+
 
 class NameForm(FlaskForm):
     name = StringField('your name',validators=[Required(),])
