d661828ad0c9abc81f2a64c5589d9310c76a806b
HymanLiu1987 <879651072@qq.com>
2016-12-15 23:03:28 +0800

    用flask开发个人博客（23）—— flask中的数据模型关系

--- test.py
+++ test.py
@@ -1,107 +1,65 @@
 #coding:utf-8
-#usr/bin/env
-from flask import Flask,session,make_response,redirect,abort,request,render_template,g,url_for,flash
-from flask_script import Manager
-from flask_moment import Moment
-from flask_wtf import FlaskForm
-from wtforms import StringField,SubmitField
-from wtforms.validators import Required
+from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
-from flask_migrate import Migrate,MigrateCommand
 import os
 basedir=os.path.abspath(os.path.dirname(__file__))
-
-
-import datetime
 app=Flask(__name__)
-app.config['SECRET_KEY']='secret_key'
-manager=Manager(app)
-moment=Moment(app)
-app.config['SQLALCHEMY_DATABASE_URI']='sqlite:///'+os.path.join(basedir+'data.sqlite')
-app.config['SQLALCHEMY_COMMIT_ON_TEARDOWN']=True
 db=SQLAlchemy(app)
-migrate=Migrate(app,db)
-manager.add_command('db',MigrateCommand)
 
 class Role(db.Model):
+    """
+    角色
+    """
     __tablename__='roles'
     id=db.Column(db.Integer,primary_key=True)
     name=db.Column(db.String(64),unique=True)
-    users=db.relationship('User',backref='role')
+    users=db.relationship('User',backref='role',uselist=True)
 
     def __repr__(self):
         return '<Role %s>'%self.name
 
-class User(db.Model):
-    __tablename__='users'
+class Identification(db.Model):
+    """
+    身份证
+    """
+    __tablename__='idents'
     id=db.Column(db.Integer,primary_key=True)
-    name=db.Column(db.String(64),unique=True)
+    address=db.Column(db.String(64),unique=True)
+    #user=db.relationship('User',backref='ident',uselist=False)
+
+    def __repr__(self):
+        return '<Identification %s>'%self.id
+
+class User(db.Model):
+    """
+    用户
+    """
+    __tablename__='users'
+    id = db.Column(db.Integer,primary_key=True)
+    name = db.Column(db.String(64),unique=True)
+    #ident_id=db.Column(db.Integer,db.ForeignKey('idents.id'))
     role_id=db.Column(db.Integer,db.ForeignKey('roles.id'))
-    age=db.Column(db.Integer)
+    
 
     def __repr__(self):
         return '<User %s>'%self.name
 
-
-class NameForm(FlaskForm):
-    name = StringField('your name',validators=[Required(),])
-    submit = SubmitField('Submit')
+relation=db.Table('relations',db.Column('student_id',db.Integer,db.ForeignKey('students.id')),db.Column('class_id',db.Integer,db.ForeignKey('classes.id')))
 
 
-@app.route('/',methods=['GET','POST'])
-def index():
-    form = NameForm()
-    if form.validate_on_submit():
-        user = User.query.filter_by(name=form.name.data).first()
-        if user is None:
-            user=User(name=form.name.data)
-            db.session.add(user)
-        session['name']=form.name.data
-        form.name.data=''
-        return redirect(url_for('index'))
-    return render_template('index.html',form=form,name=session['name'])
+class Student(db.Model):
+    __tablename__='students'
+    id=db.Column(db.Integer,primary_key=True)
+    name=db.Column(db.String)
+    classes=db.relationship('Class',secondary=relation,backref=db.backref('students',lazy='dynamic'),lazy='dynamic')
+    
+    def __repr__(self):
+        return '<Student %s>'%self.name
 
-@app.route('/set_cookie')
-def set_cookie():
-    response=make_response('Hello World');
-    outdate=datetime.datetime.today() + datetime.timedelta(days=30)
-    response.set_cookie('Name','Hyman',expires=outdate)
-    return response
+class Class(db.Model):
+    __tablename__='classes'
+    id=db.Column(db.Integer,primary_key=True)
+    name=db.Column(db.String)
 
-@app.route('/get_cookie')
-def get_cookie():
-    name=request.cookies.get('Name')
-    return name
-
-@app.route('/get_template')
-def get_template():
-    return render_template('test.html')
-
-@app.route('/moment')
-def get_moment():
-    return render_template('moment.html')
-
-@app.route('/test')
-def test():
-    return g.string
-
-@app.before_first_request
-def bf_first_request():
-    g.string = 'before_first_request'
-
-@app.before_request
-def bf_request():
-    g.string = 'before_request'
-
-@app.after_request
-def af_request(param):
-    return param
-
-@app.teardown_request
-def td_request(param):
-    return param
-
-if __name__=='__main__':
-    manager.run()
-
-
+    def __repr__(self):
+        return '<CLass %s>'%self.name
