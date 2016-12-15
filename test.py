648ef4721ea2856eb18a53b149dcf9bed2b458de
HymanLiu1987 <879651072@qq.com>
2016-12-15 23:19:49 +0800

    用flask开发个人博客（24）—— flask中使用Flask_Mail发送电子邮件

--- test.py
+++ test.py
@@ -1,65 +1,13 @@
 #coding:utf-8
 from flask import Flask
-from flask_sqlalchemy import SQLAlchemy
+from flask_mail import Mail
 import os
 basedir=os.path.abspath(os.path.dirname(__file__))
 app=Flask(__name__)
-db=SQLAlchemy(app)
+app.config['MAIL_SERVER']='smtp.qq.com'
+app.config['MAIL_PORT']=587
+app.config['MAIL_USE_TLS']=True
+app.config['MAIL_USERNAME']=os.environ.get('MAIL_USERNAME')
+app.config['MAIL_PASSWORD']=os.environ.get('MAIL_PASSWORD')
+mail=Mail(app)
 
-class Role(db.Model):
-    """
-    角色
-    """
-    __tablename__='roles'
-    id=db.Column(db.Integer,primary_key=True)
-    name=db.Column(db.String(64),unique=True)
-    users=db.relationship('User',backref='role',uselist=True)
-
-    def __repr__(self):
-        return '<Role %s>'%self.name
-
-class Identification(db.Model):
-    """
-    身份证
-    """
-    __tablename__='idents'
-    id=db.Column(db.Integer,primary_key=True)
-    address=db.Column(db.String(64),unique=True)
-    #user=db.relationship('User',backref='ident',uselist=False)
-
-    def __repr__(self):
-        return '<Identification %s>'%self.id
-
-class User(db.Model):
-    """
-    用户
-    """
-    __tablename__='users'
-    id = db.Column(db.Integer,primary_key=True)
-    name = db.Column(db.String(64),unique=True)
-    #ident_id=db.Column(db.Integer,db.ForeignKey('idents.id'))
-    role_id=db.Column(db.Integer,db.ForeignKey('roles.id'))
-    
-
-    def __repr__(self):
-        return '<User %s>'%self.name
-
-relation=db.Table('relations',db.Column('student_id',db.Integer,db.ForeignKey('students.id')),db.Column('class_id',db.Integer,db.ForeignKey('classes.id')))
-
-
-class Student(db.Model):
-    __tablename__='students'
-    id=db.Column(db.Integer,primary_key=True)
-    name=db.Column(db.String)
-    classes=db.relationship('Class',secondary=relation,backref=db.backref('students',lazy='dynamic'),lazy='dynamic')
-    
-    def __repr__(self):
-        return '<Student %s>'%self.name
-
-class Class(db.Model):
-    __tablename__='classes'
-    id=db.Column(db.Integer,primary_key=True)
-    name=db.Column(db.String)
-
-    def __repr__(self):
-        return '<CLass %s>'%self.name
