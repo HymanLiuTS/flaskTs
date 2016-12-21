c215e81744eb00a96e19c4544d7bbe2ce34b9061
HymanLiu1987 <879651072@qq.com>
2016-12-21 22:28:40 +0800

    用flask开发个人博客（28）—— 利用unittest进行单元测试

---
+++ config.py
@@ -0,0 +1,39 @@
+import os  
+basedir = os.path.abspath(os.path.dirname(__file__))  
+  
+class Config:  
+    SECRET_KEY = os.environ.get('SECRET_KEY') or 'hard to guess string'  
+    SQLALCHEMY_COMMIT_ON_TEARDOWN = True  
+    FLASKY_MAIL_SUBJECT_PREFIX='[Flasky]'  
+    FLASKY_MAIL_SENDER = '879651072@qq.com'  
+    FLASKY_ADMIN = os.environ.get('FLASKY_ADMIN')  
+ 
+    @staticmethod  
+    def init_app(app):  
+        pass  
+  
+class DevelopmentConfig(Config):  
+    DEBUG = True  
+    MAIL_SERVER = 'smtp.qq.com'  
+    MAIL_PORT = 587  
+    MAIL_USE_TLS = True  
+    MAIL_USERNAME = os.environ.get('MAIL_USERNAME')  
+    MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')  
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DEV_DATABASE_URI') or \  
+            'sqlite:///' + os.path.join(basedir,'data-dev.sqlite')  
+  
+class TestingConfig(Config):  
+    TESTING = True  
+    SQLALCHEMY_DATABASE_URI = os.environ.get('TEST_DATABASE_URI') or \  
+            'sqlite:///' + os.path.join(basedir,'data-test.sqlite')  
+  
+class ProductionConfig(Config):  
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URI') or \  
+            'sqlite:///' + os.path.join(basedir,'data.sqlite')  
+  
+config={  
+        'development':DevelopmentConfig,  
+        'testing':TestingConfig,  
+        'Production':ProductionConfig,  
+        'default':DevelopmentConfig  
+        }  
---
+++ tests/test_basic.py
@@ -0,0 +1,20 @@
+import unittest
+from flask import current_app
+from app import create_app,db
+class BasicTestCase(unittest.TestCase):
+    def Setup(self):
+        self.app=create_app('testing')
+        self.app_context=self.app.app_context()
+        self.app_context.push()
+        db.create_all()
+
+    def teardown(self):
+        db.session.remove()
+        db.drop_all()
+        self.app_context.pop()
+
+    def test_app_exits(self):
+        self.assertFalse(current_app is None)
+
+    def test_app_is_testing(self):
+        self.assertFalse(current_app.config['TESTING'])
