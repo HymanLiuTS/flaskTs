8e584431032722413f6e820fd83e38a36c0eb34e
HymanLiu1987 <879651072@qq.com>
2017-01-05 22:28:39 +0800

    用flask开发个人博客（34）—— 用户角色权限的分配

--- app/models.py
+++ app/models.py
@@ -1,15 +1,26 @@
 from app import db 
 from werkzeug.security import generate_password_hash,check_password_hash
-from flask_login import UserMixin
+from flask_login import UserMixin,AnonymousUserMixin
 from itsdangerous import TimedJSONWebSignatureSerializer as Serializer
 class Role(db.Model):  
     __tablename__='role'  
     id=db.Column(db.Integer,primary_key=True)
     name=db.Column(db.String(64),unique=True)
+    permission=db.Column(db.Integer)
     users=db.relationship('User',backref='role',lazy='dynamic')
+    
     def __repr__(self):
         return '<Role %r>'% self.name
-      
+
+class Permission:
+    FOLLOW=0x01
+    COMMENT=0x02
+    WRITE=0x04
+    MANAGE_COMMENTS=0x08
+    ADMIN=0x80
+
+
+
 class User(db.Model,UserMixin):
     __tablename__='users'
     id=db.Column(db.Integer,primary_key=True)
@@ -17,6 +28,20 @@ class User(db.Model,UserMixin):
     role_id=db.Column(db.Integer,db.ForeignKey('role.id'))
     hash_password=db.Column(db.String(28))
     confirmed=db.Column(db.Boolean,default=False)
+    
+    def __init__(self,**kwargs):
+        super(User,self).__init__(**kwargs)
+        if self.role is None:
+            if self.name == 'Hyman':
+                self.role=Role.query.filter_by(permission=0x8f).first()
+            if self.role is None:
+                self.role=Role.query.filter_by(permission=0x07).first()
+    
+    def can(self,permission):
+        return self.role is not None and (self.role.permission & permission) == permission
+
+    def is_admin(self):
+        return self.can(Permission.ADMIN)
 
     def generate_token(self,expiration=3600):
         s=Serializer('secret key',expiration)
@@ -45,9 +70,29 @@ class User(db.Model,UserMixin):
     def confirm_password(self,password):
         return check_password_hash(self.hash_password,password)
 
-
+class AnonymousUser(AnonymousUserMixin):
+    def can(self,permission):
+        return False
+    def is_admin(self):
+        return False
 
 from . import loginmanager
 @loginmanager.user_loader
 def load_user(id):
     return User.query.get(int(id))
+
+loginmanager.anonymous_user=AnonymousUser
+
+def create_roles():
+    roles={'User':Permission.FOLLOW|Permission.COMMENT|Permission.WRITE,
+            'Moderator':Permission.FOLLOW|Permission.COMMENT|Permission.WRITE|Permission.MANAGE_COMMENTS,
+            'Admin':Permission.FOLLOW|Permission.COMMENT|Permission.WRITE|Permission.MANAGE_COMMENTS|Permission.ADMIN
+            }
+    for r in roles:
+        print r
+        role = Role.query.filter_by(name=r).first()
+        if role is None:
+            role=Role(name=r)
+        role.permission=roles[r]
+        db.session.add(role)
+    db.session.commit()
--- manager.py
+++ manager.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python  
 import os  
 from app import create_app,db  
-from app.models import User,Role  
+from app.models import User,Role,create_roles  
 from flask_script import Manager,Shell  
 from flask_migrate import Migrate,MigrateCommand  
   
@@ -10,7 +10,7 @@ manager=Manager(app)
 migrate=Migrate(app,db)  
   
 def make_shell_context():  
-    return dict(app=app,db=db,User=User,Role=Role)  
+    return dict(app=app,db=db,User=User,Role=Role,create_roles=create_roles)  
   
 manager.add_command("shell",Shell(make_context=make_shell_context))  
 manager.add_command('db',MigrateCommand)  
